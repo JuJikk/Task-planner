5ae4e80c89f1315bf9d4e5ff315bb2ffc897a2c4
Marko <114610623+jujikk@users.noreply.github.com>
2024-04-07 18:25:58 +0300

    add check and delete feature

--- src/app/ui/todolist/TodoItem.tsx
+++ src/app/ui/todolist/TodoItem.tsx
@@ -1,15 +1,9 @@
-"use client";
-
 import { TodoItemProps } from "@/app/lib/defenitions";
 import React from "react";
 import ButtonsTodo from "@/app/ui/todolist/Buttons";
+import {changeServerSideProps} from "@/app/lib/action";
 
 const TodoItem: React.FC<TodoItemProps> = ({ todo,changeData }) => {
-  // const [data, setData] = useState<Todo>(todo)
-  //
-  // useEffect(() => {
-  //     changeServerSideProps(todo)
-  // }, [data])
 
   const spanStyled = todo.done
     ? "text-balance line-through text-lg font-medium text-cyan-50"
@@ -22,16 +16,23 @@ const TodoItem: React.FC<TodoItemProps> = ({ todo,changeData }) => {
           id="todo1"
           name="todo1"
           type="checkbox"
-          defaultChecked={todo.done}
+          checked={todo.done}
           className="h-4 w-4 text-teal-600 focus:ring-teal-500 border-gray-300 rounded"
+          onChange={() => {
+            changeServerSideProps(todo).then(() => changeData());
+          }}
         />
         <div className="ml-3 flex flex-row w-full justify-between">
-          {" "}
-          {/* Container for label and buttons */}
-          <label htmlFor="todo1" className="ml-3 text-gray-900 flex items-center">
+          <label
+            htmlFor="todo1"
+            className="ml-3 text-gray-900 flex items-center"
+            onClick={() => {
+
+            }}
+          >
             <span className={spanStyled}>{todo.title}</span>
           </label>
-          <ButtonsTodo changeData={changeData} todoId={todo._id}/>
+          <ButtonsTodo changeData={changeData} todoId={todo._id} />
         </div>
       </div>
     </li>
